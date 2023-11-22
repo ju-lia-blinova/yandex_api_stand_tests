1d4cab4e04033684e1ee8757e2051f3e874e1ea8
ju-lia-blinova <ju.lia.blinova@yandex.ru>
2023-11-22 22:46:35 +0300

    init commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://3ceeda12-8c89-4e9a-8968-9bdc11a0f08b.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
